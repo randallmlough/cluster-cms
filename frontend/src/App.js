fcda46af1e358a8ceda49feefbc60ef52c96ba12
Randall Lough <randallmlough@gmail.com>
2020-03-16 17:11:36 -0700

    dashboard styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
+import "./App.css";
 function App() {
   return (
     <div className="App">
