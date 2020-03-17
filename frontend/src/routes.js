321a1bf900523ce50dfa64f9eb6385a1bf589a87
Randy Lough <randallmlough@gmail.com>
2020-03-16 17:14:12 -0700

    Dashboard styling (#4)
    * Dashboard view
    
    * Navbar styled to mimick Hubspot
    
    * dashboard styling

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import { StaticView, AppView } from "./views";
 import { RegisterUserView, AuthenticateUserView } from "./views/session";
+import Dashboard from "./views/dashboard";
 import NotFound from "./views/404";
 
 export default () => {
@@ -9,6 +10,7 @@ export default () => {
     <Switch>
       <StaticView exact path={routes.REGISTER} component={RegisterUserView} />
       <StaticView exact path={routes.LOGIN} component={AuthenticateUserView} />
+      <AppView exact path={routes.DASHBOARD} component={Dashboard} />
       <StaticView path="*" component={NotFound} />
     </Switch>
   );
--- frontend/src/views/dashboard.js
+++ frontend/src/views/dashboard.js
@@ -2,7 +2,27 @@ import React from "react";
 import { connect } from "react-redux";
 
 const Dashboard = props => {
-  return <h1>Dashboard view</h1>;
+  return (
+    <main className="bg-gray-100">
+      <header className="flex justify-between bg-white border-b border-gray-300 mx-auto px-12 py-6">
+        <div>
+          <h1 className="font-thin leading-tight text-4xl text-secondary-500">
+            Dashboard
+          </h1>
+        </div>
+        <div>
+          <button className="bg-primary-500 hover:bg-primary-400 rounded-sm px-6 py-3 text-white">
+            Add contact
+          </button>
+        </div>
+      </header>
+      <section className="px-12 py-12">
+        <div className="shadow bg-white p-5 rounded">
+          <h2 className="font-light text-3xl text-gray-800">Contacts</h2>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 const mapDispatchToProps = dispatch => ({});
