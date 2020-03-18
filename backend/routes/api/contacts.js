a14241b99fb68820571882c76aa63b219a181bb5
Randall Lough <randallmlough@gmail.com>
2020-03-18 15:46:56 -0700

    fix for contact fetch bug

--- backend/routes/api/contacts.js
+++ backend/routes/api/contacts.js
@@ -1,79 +1,91 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
-const Contact = require("../../models/Contact");
+const passport = require('passport');
+const Contact = require('../../models/Contact');
 
-const validateContactInput = require('../../validation/contact')
+const validateContactInput = require('../../validation/contact');
 
 const contactParams = body => {
   const params = {};
-  ["name", "email", "company", "address"].forEach(p => {
+  ['name', 'email', 'company', 'address'].forEach(p => {
     if (body[p]) params[p] = body[p];
   });
   return params;
-}
+};
 
 router.get(
   '/:id',
-  passport.authenticate('jwt', {session: false}),
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    Contact.find({ ownerId: req.user._id, id: req.params.id })
-      .then(contact => res.json(contact));
-});
+    Contact.findOne({
+      ownerId: req.user._id,
+      _id: req.params.id,
+    }).then(contact => res.json(contact));
+  }
+);
 
 router.get(
   '/',
-  passport.authenticate('jwt', {session: false}),
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    Contact.find({ ownerId: req.user._id })
-      .then(contacts => res.json(contacts));
-});
+    Contact.find({ ownerId: req.user._id }).then(contacts =>
+      res.json(contacts)
+    );
+  }
+);
 
 router.post(
   '/',
-  passport.authenticate('jwt', {session: false}),
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const { errors, isValid } = validateContactInput(req.body);
     if (!isValid) return res.status(400).json(errors);
 
-    Contact.findOne({ email: req.body.email, ownerId: req.user._id })
-      .then(contact => {
-        if (contact) return res.status(400).json({email: "A contact exists with this email"});
-        
+    Contact.findOne({ email: req.body.email, ownerId: req.user._id }).then(
+      contact => {
+        if (contact)
+          return res
+            .status(400)
+            .json({ email: 'A contact exists with this email' });
+
         const contactDetails = contactParams(req.body);
         contactDetails.ownerId = req.user._id;
 
         const newContact = new Contact(contactDetails);
-        newContact.save()
+        newContact
+          .save()
           .then(savedContact => res.json(savedContact))
           .catch(err => console.log(err));
-      })
-});
+      }
+    );
+  }
+);
 
 router.put(
-  "/:id",
-  passport.authenticate('jwt', {session: false}),
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const { errors, isValid } = validateContactInput(req.body);
     if (!isValid) return res.status(400).json(errors);
 
     updatedContact = contactParams(req.body);
-    
+
     Contact.findOneAndUpdate(
-      { ownerId: req.user._id, _id: req.params.id }, 
+      { ownerId: req.user._id, _id: req.params.id },
       updatedContact,
-      { runValidators: true, new: true })
+      { runValidators: true, new: true }
+    )
       .then(contact => res.json(contact))
       .catch(err => console.log(err));
   }
 );
 
 router.delete(
-  "/:id",
-  passport.authenticate('jwt', {session: false}),
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Contact.findOneAndDelete({ ownerId: req.user._id, _id: req.params.id })
-      .then(() => res.json({msg: "Contact was deleted"}))
+      .then(() => res.json({ msg: 'Contact was deleted' }))
       .catch(err => console.log(err));
   }
 );
