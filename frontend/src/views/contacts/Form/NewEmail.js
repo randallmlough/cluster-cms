e84f6d4d726b826142f651c8c89830b3819c5fcf
Randall Lough <randallmlough@gmail.com>
2020-03-20 14:50:16 -0700

    Create email implementation

---
+++ frontend/src/views/contacts/Form/NewEmail.js
@@ -0,0 +1,181 @@
+import React, { useState, useRef, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { createEmail } from '../../../actions';
+
+const NewEmail = props => {
+  const { contact } = props;
+  const initialState = {
+    email: '',
+    subject: '',
+    body: '',
+    date: new Date(),
+  };
+  const [isOpen, setIsOpen] = useState(false);
+  const [message, setMessage] = useState(initialState);
+  const form = useRef();
+
+  useEffect(() => {
+    if (contact && contact.email)
+      setMessage({ ...message, email: contact.email });
+  }, [contact.email]);
+
+  const [editorState, setEditorState] = React.useState(
+    EditorState.createEmpty()
+  );
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('MESSAGE', {
+      ...message,
+      body: draftToHtml(convertToRaw(editorState.getCurrentContent())),
+    });
+    // createEmail(message)
+  };
+
+  const handleSendDate = date => {
+    setMessage({ ...message, date: date });
+  };
+  return (
+    <div
+      className={
+        'absolute bottom-0 right-0 shadow mr-4' +
+        (isOpen ? ' w-8/12' : ' w-4/12')
+      }
+    >
+      <header
+        className="p-4 bg-secondary-500 flex justify-between cursor-pointer text-sm text-white rounded-t"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div>Create Email</div>
+        <div>
+          <span>X</span>
+        </div>
+      </header>
+      {isOpen && (
+        <>
+          <section className="bg-white">
+            <form ref={form}>
+              <div className="border-b border-gray-300 px-5 pt-2 pb-1">
+                <div className="flex justify-between">
+                  <label className="-mx-2">
+                    <span className="text-xs text-gray-600 text-thin block px-2">
+                      To Email
+                    </span>
+                    <input
+                      className="text-secondary-500 text-sm px-2 border border-transparent focus:border-secondary-200 focus:outline-none"
+                      type="text"
+                      value={message.email}
+                      onChange={e =>
+                        setMessage({ ...message, email: e.target.value })
+                      }
+                    />
+                  </label>
+                  <label className="-mx-2">
+                    <span className="text-xs text-gray-600 text-thin block px-2">
+                      Date
+                    </span>
+                    <DatePicker
+                      className="text-secondary-500 text-sm w-full px-2 border border-transparent focus:border-secondary-200 focus:outline-none"
+                      popperClassName="z-50"
+                      selected={message.date}
+                      onChange={handleSendDate}
+                      showTimeSelect
+                      timeIntervals={15}
+                      timeCaption="Time"
+                      dateFormat="MMMM d, yyyy h:mm aa"
+                    />
+                  </label>
+                </div>
+                <div>
+                  <div className="-mx-2 ">
+                    <input
+                      className="text-secondary-500 text-sm px-2 py-1 w-full border border-transparent focus:border-secondary-200 focus:outline-none"
+                      type="text"
+                      placeholder="Subject"
+                      value={message.subject}
+                      onChange={e =>
+                        setMessage({ ...message, subject: e.target.value })
+                      }
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <Editor
+                  editorState={editorState}
+                  onEditorStateChange={setEditorState}
+                  toolbarClassName="text-xs"
+                  wrapperClassName="demo-wrapper"
+                  editorClassName="px-4 h-48"
+                  toolbar={{
+                    options: [
+                      'inline',
+                      'blockType',
+                      'fontSize',
+                      'fontFamily',
+                      'emoji',
+                      'list',
+                      'textAlign',
+                      'link',
+                    ],
+                    inline: {
+                      inDropdown: true,
+                      options: ['bold', 'italic', 'underline', 'strikethrough'],
+                      className: 'text-xs z-40',
+                    },
+                    emoji: { className: 'z-40' },
+                    list: { inDropdown: true, className: 'z-40' },
+                    textAlign: { inDropdown: true, className: 'z-40' },
+                    link: { inDropdown: true, className: 'z-40' },
+                    blockType: {
+                      className: 'px-1 w-20 z-40',
+                      options: [
+                        'Normal',
+                        'H1',
+                        'H2',
+                        'H3',
+                        'H4',
+                        'H5',
+                        'H6',
+                        'Blockquote',
+                      ],
+                    },
+                    fontSize: {
+                      className: 'z-40',
+                    },
+                    fontFamily: {
+                      options: [
+                        'Arial',
+                        'Georgia',
+                        'Impact',
+                        'Tahoma',
+                        'Times New Roman',
+                        'Verdana',
+                      ],
+                      className: 'w-16 z-40',
+                    },
+                  }}
+                />
+              </div>
+            </form>
+          </section>
+          <footer className="border-t border-gray-400 px-4 py-3 bg-white">
+            <button
+              className="bg-primary-500 text-xs text-white font-thin px-4 py-2 rounded"
+              onClick={handleSubmit}
+            >
+              Create email
+            </button>
+          </footer>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default NewEmail;
--- frontend/src/views/contacts/show.js
+++ frontend/src/views/contacts/show.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { Icon } from "../../components/UI";
-import ContactSidebar from "./ContactSidebar";
-import ContactFeed from "./ContactFeed";
-import { getContact } from "../../actions";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Icon } from '../../components/UI';
+import ContactSidebar from './ContactSidebar';
+import ContactFeed from './ContactFeed';
+import { getContact } from '../../actions';
+import NewEmail from './Form/NewEmail';
 
 const ContactShowView = props => {
   const { contact = {}, getContact } = props;
@@ -15,7 +16,7 @@ const ContactShowView = props => {
   }, []);
 
   useEffect(() => {
-    console.log("CONTACT", contact);
+    console.log('CONTACT', contact);
   });
   if (loading) return null;
   return (
@@ -23,8 +24,9 @@ const ContactShowView = props => {
       <aside className="col-span-1 border-r border-gray-400">
         <ContactSidebar contact={contact} />
       </aside>
-      <section className="bg-gray-200 col-span-2 py-5">
+      <section className="bg-gray-200 col-span-2 py-5 relative">
         <ContactFeed />
+        <NewEmail contact={contact} />
       </section>
       <aside className="col-span-1 border-l border-gray-400">
         right sidebar
@@ -35,12 +37,12 @@ const ContactShowView = props => {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    contact: state.entities.contacts[ownProps.match.params.id]
+    contact: state.entities.contacts[ownProps.match.params.id],
   };
 };
 
 const mapDispatchToProps = dispatch => ({
-  getContact: async contactId => await dispatch(getContact(contactId))
+  getContact: async contactId => await dispatch(getContact(contactId)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ContactShowView);
