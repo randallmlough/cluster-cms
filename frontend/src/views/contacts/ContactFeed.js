0b60e147d43406b5af068c161f50d96e39add4af
Jorge Barreto <jrbd93@gmail.com>
2020-03-23 08:46:42 -0400

    update contact feed

--- frontend/src/views/contacts/ContactFeed.js
+++ frontend/src/views/contacts/ContactFeed.js
@@ -13,7 +13,25 @@ const ContactFeed = props => {
       <Tabs>
         <div label="Activity" className="px-8">
           <h3 className="mb-4">Upcoming</h3>
-          <EmailFeed />
+          <div className="shadow rounded bg-white p-5 mb-6">
+            <div className="flex justify-between mb-3">	
+              <div>	
+                <Icon icon="envelope" className="mr-4" />	
+                <span className="font-bold text-sm">Logged Email</span>	
+              </div>	
+              <span className="text-xs">{new Date().toString()}</span>	
+            </div>	
+            <div className="px-8">	
+              <h3 className="font-bold mb-4">Hello There</h3>	
+              <p className="text-sm">	
+                Hey Brian, I heard your company is in the market for a tasty	
+                cupcake supplier. We're the number one cupcake supplier in the	
+                world. If you're interested in talking about our pricing, let me	
+                know. Yours, randy "Cupcakes not to your taste? We also sell the	
+                best muffins and donuts."	
+              </p>	
+            </div>	
+          </div>
         </div>
         <div label="Email" className="px-8">
           <div className="flex justify-end mb-8">
